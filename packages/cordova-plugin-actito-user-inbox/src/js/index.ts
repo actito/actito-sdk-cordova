cd2473f41e2a5fddbdfd8b45d30e4d25e3556745
yevheniismirnov <yevhenii.smirnov@actito.com>
2025-06-27 16:36:22 +0200

    feat(user-inbox): add module

---
+++ packages/cordova-plugin-actito-user-inbox/src/js/index.ts
@@ -0,0 +1,6 @@
+import { ActitoUserInbox } from './actito-user-inbox';
+
+export * from './models/actito-user-inbox-item';
+export * from './models/actito-user-inbox-response';
+
+export default ActitoUserInbox;
