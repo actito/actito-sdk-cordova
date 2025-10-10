9aab53ca0d9fe2355a1e73b01648e00587c40c2a
yevheniismirnov <yevhenii.smirnov@actito.com>
2025-10-10 11:50:29 +0100

    chore(sample): remove scannables

--- packages/sample/www/pages/home/views/other-features/other-features.js
+++ packages/sample/www/pages/home/views/other-features/other-features.js
@@ -1,9 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function openScannables() {
-  window.location.replace('../scannables/scannables-view.html');
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function openAssets() {
   window.location.replace('../assets/assets-view.html');
 }
